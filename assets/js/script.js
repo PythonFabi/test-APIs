c7f01e730bac2bee713f4587f4eb5d8dabca9dfa
PythonFabi <129299589+PythonFabi@users.noreply.github.com>
2023-05-25 20:39:55 -0400

    add html, css and JavaScript and test API

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+var tableBody = document.getElementById('repo-table');
+var fetchButton = document.getElementById('fetch-button');
+
+function getApi() {
+  // fetch request gets a list of all the repos for the node.js organization
+  var requestUrl = 'https://www.eventbriteapi.com/v3/users/me/?token=ARSZZXFHXTKB3LEUECU4';
+
+  fetch(requestUrl) //promise -It can be fulfilled any time --> if it succeeds it will go to .then, if it fails it will go to .catch --> error
+    .then(function (response) { //datastream
+      return response.json(); // json
+    })
+    .then(function (data) {  // .json format
+      console.log(data)
+      //Loop over the data to generate a table, each table row will have a link to the repo url
+      for (var i = 0; i < data.length; i++) {
+        // Creating elements, tablerow, tabledata, and anchor
+        var createTableRow = document.createElement('tr');
+        var tableData = document.createElement('td');
+        var link = document.createElement('a');
+
+        // Setting the text of link and the href of the link
+        link.textContent = data[i].html_url;
+        link.href = data[i].html_url;
+
+        // Appending the link to the tabledata and then appending the tabledata to the tablerow
+        // The tablerow then gets appended to the tablebody
+        tableData.appendChild(link);
+        createTableRow.appendChild(tableData);
+        tableBody.appendChild(createTableRow);
+      }
+    });
+}
+
+fetchButton.addEventListener('click', getApi);
